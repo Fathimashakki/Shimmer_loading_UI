5acb017f28c26f8342b8408c3eb389e4661aec0f
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-16 15:00:27 +0530

    Initial Commit

---
+++ Shimmer_loading_UI/Shimmer_loading_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shimmer_loading_UIApp.swift
+//  Shimmer_loading_UI
+//
+//  Created by Shaan on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Shimmer_loading_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
